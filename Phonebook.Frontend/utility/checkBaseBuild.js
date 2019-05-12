ad70a152e50fd6bcd0a0562fce6f8221b81884b3
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-05-12 03:28:16 +0200

    better check Base Build script

--- Phonebook.Frontend/utility/checkBaseBuild.js
+++ Phonebook.Frontend/utility/checkBaseBuild.js
@@ -1,39 +1,45 @@
-var hashFiles = require('hash-files');
+var crypto = require('crypto');
+var fs = require('fs');
 var https = require('https');
 
+function checksum(str, algorithm, encoding) {
+  return crypto
+    .createHash(algorithm || 'md5')
+    .update(str, 'utf8')
+    .digest(encoding || 'hex');
+}
+
+function hashFiles(files) {
+  hashes = '';
+  files.forEach(path => {
+    hashes += checksum(fs.readFileSync(path));
+  });
+  return checksum(hashes);
+}
+
 // options is optional
-hashFiles(
+const hash = hashFiles(['../package.json', '../package-lock.json', '../dev.Dockerfile']);
+
+console.log(`##vso[task.setvariable variable=imageTag;isOutput=true]${hash}`);
+const req = https.get(
   {
-    files: ['../package.json', '../package-lock.json', '../dev.Dockerfile'],
-    noGlob: true
+    hostname: 'hub.docker.com',
+    port: 443,
+    path: `/v2/repositories/danielhabenicht/test/tags/${hash}/`,
+    method: 'GET'
   },
-  function(error, hash) {
-    if (error) {
-      console.error(error);
+  res => {
+    console.log(res.statusCode);
+
+    if (res.statusCode === 404) {
+      console.log(`##vso[task.setvariable variable=changes;isOutput=true]true`);
+    } else if (res.statusCode === 200) {
+      console.log(`##vso[task.setvariable variable=changes;isOutput=true]false`);
     }
-
-    console.log(`##vso[task.setvariable variable=myOutputVar;isOutput=true]${hash}`);
-    const req = https.get(
-      {
-        hostname: 'hub.docker.com',
-        port: 443,
-        path: `/v2/repositories/danielhabenicht/test/tags/${hash}/`,
-        method: 'GET'
-      },
-      res => {
-        console.log(res.statusCode);
-
-        if (res.statusCode === 404) {
-          console.log(`##vso[task.setvariable variable=changes;isOutput=true]true`);
-        } else if (res.statusCode === 200) {
-          console.log(`##vso[task.setvariable variable=changes;isOutput=true]false`);
-        }
-      }
-    );
-
-    req.on('error', error => {
-      console.error(error);
-    });
-    req.end();
   }
 );
+
+req.on('error', error => {
+  console.error(error);
+});
+req.end();
