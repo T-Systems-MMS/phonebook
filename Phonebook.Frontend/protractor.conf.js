c8566a3a925bdd6e73c822f1bfe11851d585fd47
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-06-25 20:22:18 +0200

    Fix Frontend Builds

--- Phonebook.Frontend/protractor.conf.js
+++ Phonebook.Frontend/protractor.conf.js
@@ -1,6 +1,9 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
+// Use pupeteer
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
