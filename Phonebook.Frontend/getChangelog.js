ea54547548fec43b557449540e08ee97ece3e123
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-07-09 13:27:16 +0200

    chore(build): use standard library for request

--- Phonebook.Frontend/getChangelog.js
+++ Phonebook.Frontend/getChangelog.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const https = require('https');
 const { writeFileSync } = require('fs');
 
 /**
@@ -7,21 +7,43 @@ const { writeFileSync } = require('fs');
  * node getChangelog.js <version-string>
  */
 
-lastVersion = process.argv[2];
+let lastVersion = process.argv[2];
 
 let url = `https://github.com/T-Systems-MMS/phonebook/releases/download/v${lastVersion}/changelog.md`;
 
-request.get(url, (err, res, body) => {
-  if (err) {
-    throw new Error('Failed to fetch Changelog!', err);
-  }
-  if (res.statusCode === 200) {
-    writeFileSync('src/changelog.md', body, { encoding: 'utf-8' });
-    console.log(`Fetched Changelog from: ${url}`);
-  } else {
-    throw new Error(
-      `File in remote location does not seem to exist. Error Code: ${res.statusCode} ${res.statusMessage} (${url})`,
-      err
-    );
-  }
-});
+getChangelogFromUrl(url);
+
+function getChangelogFromUrl(url) {
+  https
+    .get(url, res => {
+      let data = '';
+
+      res.on('data', chunk => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      res.on('end', () => {
+        if (res.statusCode > 300 && res.statusCode < 400) {
+          console.log(`Redirected from ${url}`);
+          if (res.headers.location) {
+            getChangelogFromUrl(res.headers.location);
+          } else {
+            throw new Error(`Could not parse redirect url.`);
+          }
+        } else if (res.statusCode === 200) {
+          writeFileSync('src/changelog.md', data, { encoding: 'utf-8' });
+          console.log(`Fetched Changelog from: ${url}`);
+        } else {
+          throw new Error(
+            `File in remote location does not seem to exist. Error Code: ${res.statusCode} ${
+              res.statusMessage
+            } (${url})`
+          );
+        }
+      });
+    })
+    .on('error', err => {
+      throw new Error('Failed to fetch Changelog!', err);
+    });
+}
