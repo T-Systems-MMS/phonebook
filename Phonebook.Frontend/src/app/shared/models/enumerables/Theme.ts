01445bf5abfa5ec6bc7c478ae7860d81d904b6b7
DanielHabenicht <daniel-habenicht@outlook.de>
2019-07-11 09:46:44 +0200

    feat(app): add Color Theme Switcher (#38)
    You can now choose between different themes to give the phonebook your personal look. This will also work beyond sessions. Try out the new Feature in your Settings.
    
    Resolves: #37

---
+++ Phonebook.Frontend/src/app/shared/models/enumerables/Theme.ts
@@ -0,0 +1,6 @@
+export enum Theme {
+    blue_light_theme = 'blue_light_theme',
+    blue_dark_theme = 'blue_dark_theme',
+    magenta_light_theme = 'magenta_light_theme',
+    magenta_dark_theme = 'magenta_dark_theme'
+}
