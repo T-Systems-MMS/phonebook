b7c572502a10813f002f10f366ef8e1888ec4d62
DanielHabenicht <daniel-habenicht@outlook.de>
2019-07-11 14:13:16 +0200

    fix(settings): load theme if page was loaded before (#83)
    resolves #82

---
+++ Phonebook.Frontend/src/migration/migrations/3/migration3.ts
@@ -0,0 +1,20 @@
+import { Migration } from 'src/migration/Migration';
+
+const APPSTATE_KEY: string = 'appstate';
+export const migration3: Migration = {
+  name: 'Add activeTheme Property to AppState',
+  id: 3,
+  script: () => {
+    const appStateString = localStorage.getItem(APPSTATE_KEY);
+    if (appStateString) {
+      const appState: any = JSON.parse(appStateString);
+      if (appState.activeTheme === undefined) {
+        const newAppState = {
+          ...appState,
+          activeTheme: 'magenta_light_theme'
+        };
+        localStorage.setItem(APPSTATE_KEY, JSON.stringify(newAppState));
+      }
+    }
+  }
+};
