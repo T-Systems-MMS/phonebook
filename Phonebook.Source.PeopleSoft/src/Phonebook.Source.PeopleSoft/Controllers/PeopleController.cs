f81c9f2843c39bccd68519982b9ff1b7698393d3
paule96 <Paul-Jeschke@outlook.com>
2019-10-07 17:45:09 +0200

    include as many informations as possible to a person

--- Phonebook.Source.PeopleSoft/src/Phonebook.Source.PeopleSoft/Controllers/PeopleController.cs
+++ Phonebook.Source.PeopleSoft/src/Phonebook.Source.PeopleSoft/Controllers/PeopleController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Oracle.EntityFrameworkCore.Storage.Internal;
 using Phonebook.Source.PeopleSoft.Models;
 
 namespace Phonebook.Source.PeopleSoft.Controllers
@@ -34,39 +35,90 @@ namespace Phonebook.Source.PeopleSoft.Controllers
         {
             return query
                     .AsNoTracking()
+                    .Include(p => p.Status)
+                    .Include(p => p.Function)
                     .Include(p => p.OrgUnit)
-                    .Include(p => p.Room)                    
+                        .ThenInclude(o => o.Parent)
+                    .Include(p => p.Room)
+                        .ThenInclude(r => r.BuildingPart)
+                            .ThenInclude(b => b.Building)
+                                .ThenInclude(b => b.Location)
+                    .Include(p => p.Room)
+                        .ThenInclude(r => r.Floor)
+                    .Include(p => p.Room)
+                    .OrderBy(p => p.Id)
+                    .ToList()
                     // the following select will remove cicle references
                     .Select(p =>
-                    new Person() {
-                      Id= p.Id,
-                      EMail = p.EMail,
-                      FAX = p.FAX,
-                      FirstName = p.FirstName,
-                      FunctionId = p.FunctionId,
-                      LastName = p.LastName,
-                      MobilPhone = p.MobilPhone,
-                      OrgUnit = p.OrgUnit != null ? new OrgUnit(){
-                        CostCenter = p.OrgUnit.CostCenter,
-                        Id = p.OrgUnit.Id,
-                        Name = p.OrgUnit.Name,
-                        ShortName = p.ShortName
-                      } : null,
-                      OrgUnitId = p.OrgUnitId,
-                      Phone = p.Phone,
-                      Room = p.Room != null ? new Room(){
-                          Id = p.Room.Id,
-                          BuildingPartId = p.Room.BuildingPartId,
-                          FloorId = p.Room.FloorId,
-                          Map = p.Room.Map,
-                          Number = p.Room.Number
-                      } : null,
-                      ShortName = p.ShortName,
-                      StatusId = p.StatusId,
-                      Title = p.Title,
-                      RoomId = p.RoomId
+                    new Person()
+                    {
+                        Id = p.Id,
+                        EMail = p.EMail,
+                        FAX = p.FAX,
+                        FirstName = p.FirstName,
+                        FunctionId = p.FunctionId,
+                        Function = p.Function != null ? new Function() { Id = p.Function.Id, Label = p.Function.Label, Code = p.Function.Code }: null,
+                        LastName = p.LastName,
+                        MobilPhone = p.MobilPhone,
+                        OrgUnit = p.OrgUnit != null ? createOrgUnitTree(p.OrgUnit) : null,
+                        OrgUnitId = p.OrgUnitId,
+                        Phone = p.Phone,
+                        Room = p.Room != null ? createRoomTree(p.Room) : null,
+                        ShortName = p.ShortName,
+                        StatusId = p.StatusId,
+                        Status = p.Status != null ? new Status() { Id = p.Status.Id, Name = p.Status.Name, Code = p.Status.Code}: null,
+                        Title = p.Title,
+                        RoomId = p.RoomId
                     });
 
+        }
+
+        private Room createRoomTree(Room room)
+        {
+            var result = new Room();
+            result.BuildingPart = room.BuildingPart != null ? new BuildingPart()
+            {
+                Description = room.BuildingPart.Description,
+                Id = room.BuildingPart.Id,
+                BuildingId = room.BuildingPart.BuildingId,
+                Building = room.BuildingPart.Building != null ? new Building() {
+                     Address = room.BuildingPart.Building.Address,
+                     Id = room.BuildingPart.Building.Id,
+                     Name = room.BuildingPart.Building.Name,
+                     ShortName = room.BuildingPart.Building.ShortName,
+                     LocationId = room.BuildingPart.Building.LocationId,
+                     Location = room.BuildingPart.Building.Location != null ? new Location() { 
+                         ShortName = room.BuildingPart.Building.Location.ShortName,
+                         Name = room.BuildingPart.Building.Location.Name,
+                         Country = room.BuildingPart.Building.Location.Country,
+                         Id = room.BuildingPart.Building.Location.Id
+                     } : null
+                }: null
+            }: null;
+            result.Id = room.Id;
+            result.FloorId = room.FloorId;
+            result.Map = room.Map;
+            result.Number = room.Number;
+            result.Floor = room.Floor != null ? new Floor()
+            {
+                Id = room.Floor.Id,
+                BuildingId = room.Floor.BuildingId,
+                Describtion = room.Floor.Describtion                
+            } : null;
+
+            return result;
+        }
+
+        private OrgUnit createOrgUnitTree(OrgUnit orgUnit)
+        {
+            var result = new OrgUnit();
+            result.Id = orgUnit.Id;
+            result.Name = orgUnit.Name;
+            result.Parent = orgUnit.Parent != null ? createOrgUnitTree(orgUnit.Parent) : null;
+            result.ParentId = orgUnit.ParentId;
+            result.ShortName = orgUnit.ShortName;
+            result.CostCenter = orgUnit.CostCenter;
+            return result;
         }
     }
 }
