c5a78c4fd43db5e5f447adbef965f920b49cee92
Zeroks77 <dominik@mangatter.de>
2019-12-02 14:08:08 +0100

    add translation for mail

--- Phonebook.Frontend/src/app/shared/dialogs/user-information/incorrect-user-information.component.ts
+++ Phonebook.Frontend/src/app/shared/dialogs/user-information/incorrect-user-information.component.ts
@@ -4,6 +4,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { IncorrectUserInformationDialogData } from 'src/app/shared/components/user/user-detail/user-detail.component';
 import { WindowRef } from 'src/app/services/windowRef.service';
 import { MailService } from 'src/app/services/mail.service';
+import { I18n } from '@ngx-translate/i18n-polyfill';
 
 @Component({
   selector: 'app-user-information',
@@ -14,6 +15,7 @@ export class IncorrectUserInformationComponent implements OnInit {
   public currentUserId: string = '';
   public mailBody : string = '';
   constructor(
+    private i18n: I18n,
     private mailService: MailService,
     public windowRef: WindowRef,
     public currentUserService: CurrentUserService,
@@ -37,8 +39,20 @@ export class IncorrectUserInformationComponent implements OnInit {
   }
   public sendMail() {
     this.mailService.openMail(
-      'There is an Issue with your Phonebook Profil',
-      'Hi ' + this.data.person.Firstname + ', \n while browsing your profile I noticed that something is not right: \n\n\n Please contact the HR Department to fix it. This is the Phonebook Link: ' + this.windowRef.getCurrentUrl(),
+      this.i18n({
+        meaning: 'MailToMateSubject',
+        description: 'Send a mail to your mate if there is something wrong on the profile ',
+        id: 'User-InformationDialogSubject',
+        value: 'There is an Issue with your Phonebook Profil'
+      }),
+      'Hi ' + this.data.person.Firstname +
+      this.i18n({
+        meaning: 'MailToMateBody',
+        description: 'Send a mail to your mate if there is something wrong on the profile ',
+        id: 'User-InformationDialogBody',
+        value: ', \n while browsing your profile I noticed that something is not right: \n\n\n Please contact the HR Department to fix it. This is the Phonebook Link: '
+      })
+      + this.windowRef.getCurrentUrl(),
       this.data.person.Contacts.Email
     );
   }
