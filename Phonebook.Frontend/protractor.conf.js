1a054046c3d0d6498690c687ffb86dbc550feb73
DanielHabenicht <daniel-habenicht@outlook.de>
2019-12-19 18:51:19 +0100

    Update protractor.conf.js

--- Phonebook.Frontend/protractor.conf.js
+++ Phonebook.Frontend/protractor.conf.js
@@ -1,8 +1,8 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-// Use pupeteer
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+console.log(process.env.CHROME_BIN);
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
@@ -11,7 +11,8 @@ exports.config = {
   specs: ['./e2e/**/*.e2e-spec.ts'],
   capabilities: {
     browserName: 'chrome',
-    binary: process.env.CHROME_BIN,
+    // Use puppeteer
+    binary: require('puppeteer').executablePath(),
     chromeOptions: {
       args: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
     }
