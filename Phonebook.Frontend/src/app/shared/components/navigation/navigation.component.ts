abd87a78a6a10ceddd5ed973b93a90b63a1332ef
Zeroks77 <dominik@mangatter.de>
2020-03-05 17:58:18 +0100

    add check for first april

--- Phonebook.Frontend/src/app/shared/components/navigation/navigation.component.ts
+++ Phonebook.Frontend/src/app/shared/components/navigation/navigation.component.ts
@@ -36,7 +36,8 @@ export class NavigationComponent implements OnInit, OnDestroy {
   public tableResultCount$: Observable<number>;
   public displayTableSettings: boolean = false;
   public hasImage: boolean = false;
-
+  public isFirstApril: boolean = false;
+  
   public currentUser: Person | null = null;
   constructor(
     private currentUserService: CurrentUserService,
@@ -70,6 +71,12 @@ export class NavigationComponent implements OnInit, OnDestroy {
       });
     this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(route => {
       this.displayTableSettings = this.router.url.includes('search');
+    });
+    this.featureFlagService
+    .get('firstApril')
+    .pipe(untilComponentDestroyed(this))
+    .subscribe(flag => {
+      this.isFirstApril = flag;
     });
   }
 
