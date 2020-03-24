bd6953fe6df2749fd5443eea52c28a5d0b2cd41b
paule96 <Paul-Jeschke@outlook.com>
2020-03-24 14:44:13 +0100

    fixing build error

--- Phonebook.Frontend/src/app/shared/models/classes/Messenger.ts
+++ Phonebook.Frontend/src/app/shared/models/classes/Messenger.ts
@@ -1,9 +1,9 @@
 /* tslint:disable:variable-name */
 export class Messenger {
   public Text?: any;
-  public State: number;
+  public State?: number;
 
-  constructor(text: any = null, state: number) {
+  constructor(text: any = null, state: number | null) {
     this.Text = text;
     this.State = state;
   }
