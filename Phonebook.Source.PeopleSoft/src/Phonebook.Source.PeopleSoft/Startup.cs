11fc4dd5389156cca6f383f86d2a9903cb71670e
paule96 <Paul-Jeschke@outlook.com>
2020-03-25 14:49:15 +0100

    add an option to run the backend without an auth process

--- Phonebook.Source.PeopleSoft/src/Phonebook.Source.PeopleSoft/Startup.cs
+++ Phonebook.Source.PeopleSoft/src/Phonebook.Source.PeopleSoft/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Primitives;
-using Phonebook.Source.PeopleSoft.Models;
 using Phonebook.Source.PeopleSoft.Models.Context;
 using System;
 using System.Collections.Generic;
@@ -40,7 +39,7 @@ namespace Phonebook.Source.PeopleSoft
                         .ConfigureWarnings(w => w.Throw(RelationalEventId.QueryClientEvaluationWarning))
                         .EnableSensitiveDataLogging(true)
                         );
-                
+
             }
             else
             {
@@ -54,6 +53,25 @@ namespace Phonebook.Source.PeopleSoft
 #if DEBUG
             Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
 #endif
+            if (Configuration.GetValue<bool>("useAnonymos"))
+            {
+                if (!Configuration.GetValue<bool>("useSeeding"))
+                {
+                    throw new NotSupportedException("It is not supported to use anonymos auth and real data! Please also enable 'useSeeding'!");
+                }
+                services
+                    .AddAuthentication("noAuth")
+                    .AddScheme<NoAuthOptions, NoAuthHandler>("noAuth", o => { });
+            }
+            else
+            {
+                AddWsFederation(services);
+            }
+        }
+
+
+        private void AddWsFederation(IServiceCollection services)
+        {
             services.AddAuthentication(sharedOptions =>
             {
                 sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
@@ -61,80 +79,78 @@ namespace Phonebook.Source.PeopleSoft
                 sharedOptions.DefaultChallengeScheme = WsFederationDefaults.AuthenticationScheme;
 
             })
-              .AddWsFederation(options =>
-              {
-                  // MetadataAddress represents the Active Directory instance used to authenticate users.
-                  options.MetadataAddress = Configuration.GetValue<string>("WsFederationConfig:MetadataAddress"); ;
+                          .AddWsFederation(options =>
+                          {
+                              // MetadataAddress represents the Active Directory instance used to authenticate users.
+                              options.MetadataAddress = Configuration.GetValue<string>("WsFederationConfig:MetadataAddress"); ;
 
-                  var appId = Configuration.GetValue<string>("WsFederationConfig:ApplicationId");
-
-
-                  // Wtrealm is the app's identifier in the Active Directory instance.
-                  // For ADFS, use the relying party's identifier, its WS-Federation Passive protocol URL:                  
-                  options.Wtrealm = $"{appId}";
-                  options.Wtrealm = $"spn:{appId}";
-                  options.AllowUnsolicitedLogins = true;
-                  // https://stackoverflow.com/questions/28627061/owin-ws-federation-setting-up-token-sliding-expiration
-                  options.UseTokenLifetime = false;
+                              var appId = Configuration.GetValue<string>("WsFederationConfig:ApplicationId");
 
 
+                              // Wtrealm is the app's identifier in the Active Directory instance.
+                              // For ADFS, use the relying party's identifier, its WS-Federation Passive protocol URL:                  
+                              options.Wtrealm = $"{appId}";
+                              options.Wtrealm = $"spn:{appId}";
+                              options.AllowUnsolicitedLogins = true;
+                              // https://stackoverflow.com/questions/28627061/owin-ws-federation-setting-up-token-sliding-expiration
+                              options.UseTokenLifetime = false;
 
-                  options.ClaimsIssuer = Configuration.GetValue<string>("WsFederationConfig:ClaimsIssuer");
-                  options.Events.OnRedirectToIdentityProvider = (c) =>
-                  {
-                      var hostname = string.Empty;
-                      if (string.IsNullOrWhiteSpace(c.Request.Headers["Referer"]) == false)
-                      {
-                          var uri = new Uri(c.Request.Headers["Referer"]);
-                          hostname = $"{uri.Host}:{uri.Port}";
-                      }
-                      else
-                      {
-                          hostname = c.Request.Host.ToString();
-                      }
-                      c.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-                      c.ProtocolMessage.Wreply = $"https://{hostname}/signin-wsfed";
-                      c.Properties.RedirectUri = $"{c.Request.Headers["Referer"]}";
-                      c.Response.Headers.Add(new KeyValuePair<string, StringValues>("Location", new StringValues(c.ProtocolMessage.CreateSignInUrl())));
-                      c.HandleResponse();
-                      return Task.CompletedTask;
-                  };
+                              options.ClaimsIssuer = Configuration.GetValue<string>("WsFederationConfig:ClaimsIssuer");
+                              options.Events.OnRedirectToIdentityProvider = (c) =>
+                              {
+                                  var hostname = string.Empty;
+                                  if (string.IsNullOrWhiteSpace(c.Request.Headers["Referer"]) == false)
+                                  {
+                                      var uri = new Uri(c.Request.Headers["Referer"]);
+                                      hostname = $"{uri.Host}:{uri.Port}";
+                                  }
+                                  else
+                                  {
+                                      hostname = c.Request.Host.ToString();
+                                  }
+                                  c.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                                  c.ProtocolMessage.Wreply = $"https://{hostname}/signin-wsfed";
+                                  c.Properties.RedirectUri = $"{c.Request.Headers["Referer"]}";
+                                  c.Response.Headers.Add(new KeyValuePair<string, StringValues>("Location", new StringValues(c.ProtocolMessage.CreateSignInUrl())));
+                                  c.HandleResponse();
+                                  return Task.CompletedTask;
+                              };
 
-                  options.Events.OnAuthenticationFailed = (c) =>
-                  {
-                      var hostname = string.Empty;
-                      if (string.IsNullOrWhiteSpace(c.Request.Headers["Referer"]) == false)
-                      {
-                          var uri = new Uri(c.Request.Headers["Referer"]);
-                          hostname = $"{uri.Host}:{uri.Port}";
-                      }
-                      else
-                      {
-                          hostname = c.Request.Host.ToString();
-                      }
-                      c.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-                      c.ProtocolMessage.Wreply = $"https://{hostname}/signin-wsfed";
-                      c.Properties.RedirectUri = $"{c.Request.Headers["Referer"]}";
-                      c.Response.Headers.Add(new KeyValuePair<string, StringValues>("Location", new StringValues(c.ProtocolMessage.CreateSignInUrl())));
-                      c.HandleResponse();
-                      return Task.CompletedTask;
-                  };
+                              options.Events.OnAuthenticationFailed = (c) =>
+                              {
+                                  var hostname = string.Empty;
+                                  if (string.IsNullOrWhiteSpace(c.Request.Headers["Referer"]) == false)
+                                  {
+                                      var uri = new Uri(c.Request.Headers["Referer"]);
+                                      hostname = $"{uri.Host}:{uri.Port}";
+                                  }
+                                  else
+                                  {
+                                      hostname = c.Request.Host.ToString();
+                                  }
+                                  c.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                                  c.ProtocolMessage.Wreply = $"https://{hostname}/signin-wsfed";
+                                  c.Properties.RedirectUri = $"{c.Request.Headers["Referer"]}";
+                                  c.Response.Headers.Add(new KeyValuePair<string, StringValues>("Location", new StringValues(c.ProtocolMessage.CreateSignInUrl())));
+                                  c.HandleResponse();
+                                  return Task.CompletedTask;
+                              };
 
-                  // For AAD, use the App ID URI from the app registration's Properties blade:
-                  //options.Wtrealm = "https://wsfedsample.onmicrosoft.com/63e4796f-9e4e-40f1-928b-d4efd0642d0d";
+                              // For AAD, use the App ID URI from the app registration's Properties blade:
+                              //options.Wtrealm = "https://wsfedsample.onmicrosoft.com/63e4796f-9e4e-40f1-928b-d4efd0642d0d";
 
-              })
-              .AddCookie();
+                          })
+                          .AddCookie();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
             if (Configuration.GetValue<bool>("useSeeding"))
-            {              
+            {
                 services.GetRequiredService<ModelContext>().Database.EnsureCreated();
             }
-            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
