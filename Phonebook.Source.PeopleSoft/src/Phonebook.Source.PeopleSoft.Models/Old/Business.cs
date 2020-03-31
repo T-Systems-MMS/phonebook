bd6a91601710218adce4fe3c9ab5c772dd3c0973
paule96 <Paul-Jeschke@outlook.com>
2020-03-31 12:08:57 +0200

    fixing bug for the supervisor link

--- Phonebook.Source.PeopleSoft/src/Phonebook.Source.PeopleSoft.Models/Old/Business.cs
+++ Phonebook.Source.PeopleSoft/src/Phonebook.Source.PeopleSoft.Models/Old/Business.cs
@@ -8,7 +8,7 @@ namespace Phonebook.Source.PeopleSoft.Models.Old
     public class Business
     {
         private readonly OrgUnit orgUnit;
-        private readonly IEnumerable<OrgUnit> orgUnitStructur;        
+        private readonly IEnumerable<OrgUnit> orgUnitStructur;
 
         public Business(Models.OrgUnit orgUnit, bool forOwners = false)
         {
@@ -31,11 +31,11 @@ namespace Phonebook.Source.PeopleSoft.Models.Old
             }
         }
         public IEnumerable<string?>? ShortBusinessunitTeamassistent { get { return orgUnit?.OrgUnitToFunctions?.Where(d => d.RoleName == "TA")?.Select(d => d?.Person?.ShortName); } }
-        public IEnumerable<string?>? ShortSupervisor { get { return orgUnitStructur?.Where(d => d?.Id != 110).Select(d => d?.HeadOfOrgUnit?.ShortName); } }
-        public IEnumerable<string?>? ShortOrgUnit { get { return orgUnitStructur?.Where(d => d?.Id != 110).Select(d => d?.ShortName); } }
-        public IEnumerable<string?>? OrgUnit { get { return orgUnitStructur?.Where(d => d?.Id != 110).Select(d => d?.Name); } }
+        public IEnumerable<string?>? ShortSupervisor { get { return new string[] { orgUnitStructur?.Where(d => d?.Id != 110 && d?.HeadOfOrgUnit != null).Select(d => d?.HeadOfOrgUnit?.ShortName).LastOrDefault() }; } }
+        public IEnumerable<string?>? ShortOrgUnit { get { return orgUnitStructur?.Where(d => d?.Id != 110 && string.IsNullOrWhiteSpace(d?.HeadOfOrgUnit?.ShortName) == false).Select(d => d?.ShortName); } }
+        public IEnumerable<string?>? OrgUnit { get { return orgUnitStructur?.Where(d => d?.Id != 110 && d?.HeadOfOrgUnit != null).Select(d => d?.Name); } }
         public IEnumerable<string> BusinessunitTeamassistent { get { return orgUnit.OrgUnitToFunctions.Where(d => d.RoleName == "TA").Select(d => $"{d.Person.FirstName} {d.Person.LastName}"); } }
-        public IEnumerable<string> Supervisor { get { return orgUnitStructur.Where(d => d.Id != 110).Select(d => $"{d?.HeadOfOrgUnit?.FirstName} {d?.HeadOfOrgUnit?.LastName}"); } }
+        public IEnumerable<string> Supervisor { get { return new string[] { orgUnitStructur.Where(d => d.Id != 110 && string.IsNullOrWhiteSpace(d?.HeadOfOrgUnit?.FirstName) == false && string.IsNullOrWhiteSpace(d?.HeadOfOrgUnit?.LastName) == false).Select(d => $"{d?.HeadOfOrgUnit?.FirstName} {d?.HeadOfOrgUnit?.LastName}").LastOrDefault() }; } }
         public string? Costcenter { get { return orgUnit?.CostCenter; } }
     }
 }
