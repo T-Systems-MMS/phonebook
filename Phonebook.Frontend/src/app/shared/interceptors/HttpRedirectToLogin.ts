584ef296fb24b2098f6fe6f3b387ab8141d1b9cd
paule96 <paul-jeschke@outlook.com>
2020-04-14 15:45:10 +0200

    fix(frontend): Safarie redirection loop (#625)
    * Update HttpRedirectToLogin.ts
    
    * Update HttpRedirectToLogin.ts

--- Phonebook.Frontend/src/app/shared/interceptors/HttpRedirectToLogin.ts
+++ Phonebook.Frontend/src/app/shared/interceptors/HttpRedirectToLogin.ts
@@ -28,7 +28,7 @@ export class HttpRedirectToLogin implements HttpInterceptor {
         }
       }),
       catchError((err, d) => {
-        if (err instanceof HttpErrorResponse && err.status == 401) {
+        if (err instanceof HttpErrorResponse && err.status == 401 && err.headers.has('Location')) {
           const location = err.headers.get('Location') as string;
           window.location.replace(location);
         }
