736a5838a56c792a3cca336601401b42fb114928
friedaxvictoria <61278471+friedaxvictoria@users.noreply.github.com>
2020-05-22 16:58:30 +0200

    fix(table): Progress Spinner and Error Messages (#674)
    * spinner is working
    
    * error
    
    * no Columns error
    
    * fix(interceptor): throw error on failed requests
    
    * fix(progress-spinner): feedvback from pr
    
     - add retry button
     - remove unused css
     - fix table column error not displayed because table is visible
     - remove extra loading property
    
    * trans(table): update translations
    
    * noColumns message not displayed when error
    
    * Update Phonebook.Frontend/src/app/modules/table/components/table/table.component.ts
    
    Co-authored-by: Daniel Habenicht <daniel-habenicht@outlook.de>

--- Phonebook.Frontend/src/app/modules/table/components/table/table.component.ts
+++ Phonebook.Frontend/src/app/modules/table/components/table/table.component.ts
@@ -11,6 +11,7 @@ import { PersonService } from 'src/app/services/api/person.service';
 import { ColumnDefinitions } from 'src/app/shared/config/columnDefinitions';
 import { Person, PhonebookSortDirection, TableSort } from 'src/app/shared/models';
 import { SearchState, SetTableResultCount, TableState, UpdateUrl } from 'src/app/shared/states';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-table',
@@ -43,6 +44,8 @@ export class TableComponent implements OnInit, OnDestroy {
   }
   public sortDirection: SortDirection = '';
   public sortActive: string = '';
+
+  public inErrorState: boolean = false;
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -52,25 +55,7 @@ export class TableComponent implements OnInit, OnDestroy {
   ) {}
 
   public ngOnInit() {
-    this.personService.getAll().subscribe((persons) => {
-      this.dataSource = new PersonsDataSource(persons);
-
-      // Defer until Data is loaded.
-      this.refreshTableSubscription = merge(
-        this.store.select(SearchState.searchTerm),
-        this.store.select(SearchState.searchFilters)
-      )
-        .pipe(
-          // Debounce Time is this low, as it just aims to bundle all Observables above, especially at first page load,
-          // where all three fire as they are initialized.
-          debounceTime(50)
-        )
-        .subscribe((val) => {
-          this.refreshTable();
-          this.dataSource.pageSize = this.initialPageSize;
-        });
-    });
-
+    this.loadPersonData();
     this.route.queryParamMap.pipe(untilComponentDestroyed(this)).subscribe((queryParams) => {
       // Table Sort
       const sortDirection = queryParams.get('sortDirection');
@@ -90,6 +75,33 @@ export class TableComponent implements OnInit, OnDestroy {
       );
       this.refreshTable();
     });
+  }
+
+  public loadPersonData(ignoreCache: boolean = false) {
+    this.inErrorState = false;
+    this.personService.getAll(ignoreCache).subscribe(
+      (persons) => {
+        this.dataSource = new PersonsDataSource(persons);
+
+        // Defer until Data is loaded.
+        this.refreshTableSubscription = merge(
+          this.store.select(SearchState.searchTerm),
+          this.store.select(SearchState.searchFilters)
+        )
+          .pipe(
+            // Debounce Time is this low, as it just aims to bundle all Observables above, especially at first page load,
+            // where all three fire as they are initialized.
+            debounceTime(50)
+          )
+          .subscribe((val) => {
+            this.refreshTable();
+            this.dataSource.pageSize = this.initialPageSize;
+          });
+      },
+      (err: HttpErrorResponse) => {
+        this.inErrorState = true;
+      }
+    );
   }
 
   public refreshTable() {
@@ -120,7 +132,9 @@ export class TableComponent implements OnInit, OnDestroy {
   }
 
   public ngOnDestroy() {
-    this.refreshTableSubscription.unsubscribe();
+    if (this.refreshTableSubscription) {
+      this.refreshTableSubscription.unsubscribe();
+    }
   }
 
   public ngAfterViewInit() {
